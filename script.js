7bc83440ba7f0bfa43297d4acf083ca216692938
ludivineclement <ludivine.clement0985@gmail.com>
2023-07-23 13:34:21 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const slides = document.querySelectorAll(".quote-slide");
+  let currentSlide = 0;
+
+  function showSlide() {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }
+
+  showSlide();
+
+  setInterval(showSlide, 5000);
+});
+
+

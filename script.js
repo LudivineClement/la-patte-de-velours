4316230806caeed7a445fc1036c49c9d3f673396
ludivineclement <ludivine.clement0985@gmail.com>
2023-07-24 22:36:31 +0200

    modifs_animation_intro

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-const slidingDiscoverRace =  document.querySelector('.discover_race');
+const slidingDiscoverRace = document.querySelector('.discover_race');
 
-window.addEventListener('scroll', ()=> {
-  const {scrollTop, clientHeight} = document.documentElement;
+window.addEventListener('scroll', () => {
+  const { scrollTop, clientHeight } = document.documentElement;
 
-  const topElementToTopViewport= slidingDiscoverRace.getBoundingClientRect().top;
+  const topElementToTopViewport = slidingDiscoverRace.getBoundingClientRect().top;
 
   if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.70) {
     slidingDiscoverRace.classList.add('active')
+  } else {
+    slidingDiscoverRace.classList.remove('active');
   }
 })
 
 const animateElements = document.querySelectorAll('.animate-from-right, .animate-from-left, .animate-from-bottom');
 
 window.addEventListener('scroll', () => {
-  const { scrollTop, scrollHeight } = document.documentElement;
+  const { clientHeight } = document.documentElement;
 
-  animateElements.forEach((element) => {
+  animateElements.forEach(element => {
     const topElementToTopViewport = element.getBoundingClientRect().top;
 
-    if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - scrollHeight * 0.5) {
+    if (topElementToTopViewport <= clientHeight * 0.70) {
       element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+    
+    if (topElementToTopViewport <= clientHeight * 0.75) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+
+    // Et pour le troisième élément :
+    if (topElementToTopViewport <= clientHeight * 0.89) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
     }
   });
 });
+
 
 
 
